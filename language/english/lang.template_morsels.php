5fe9fb4388d5ec9d0fad358e2fbec39718b70eed
Kelsey Martens <kelsey@solspace.com>
2012-10-11 19:52:36 -0500

    Initial commit

---
+++ language/english/lang.template_morsels.php
@@ -0,0 +1,239 @@
+<?php
+
+ /**
+ * Solspace - Template Morsels
+ *
+ * @package		Solspace:Template Morsels
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2008-2012, Solspace, Inc.
+ * @link		http://solspace.com/docs/addon/c/Template_Morsels/
+ * @version		1.6.0
+ * @filesource 	./system/expressionengine/third_party/template_morsels/language/english/
+ */
+
+$L = $lang = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"template_morsels_module_name" =>
+"Template Morsels",
+
+"template_morsels_module_description" =>
+"Tasty cached morsels of code for your EE Templates",
+
+'template_morsels_module_version' =>
+'Template Morsels',
+
+//----------------------------------------
+//  Main Menu
+//----------------------------------------
+
+'homepage' =>
+"Homepage",
+
+'manage_morsels' =>
+"Manage Morsels",
+
+'online_documentation' =>
+"Online Documentation",
+
+//----------------------------------------
+//  Buttons
+//----------------------------------------
+
+'save' =>
+"Save",
+
+//----------------------------------------
+//  Errors
+//----------------------------------------
+
+'invalid_request' =>
+"Invalid Request",
+
+'template_morsels_module_disabled' =>
+"The Template Morsels module is currently disabled.  Please insure it is installed and up to date by going 
+to the module's control panel in the ExpressionEngine Control Panel",
+
+'disable_module_to_disable_extension' =>
+"To disable this extension, you must disable its corresponding <a href='%url%'>module</a>.",
+
+'enable_module_to_enable_extension' =>
+"To enable this extension, you must install its corresponding <a href='%url%'>module</a>.",
+
+'cp_jquery_requred' =>
+"The 'jQuery for the Control Panel' extension must be <a href='%extensions_url%'>enabled</a> to use this module.",
+
+//----------------------------------------
+//  Update routine
+//----------------------------------------
+
+'update_template_morsels_module' =>
+"Update Template Morsels Module",
+
+'template_morsels_update_message' =>
+"You have recently uploaded a new version of Template Morsels, please click here to run the update script.",
+
+"update_successful" =>
+"The module was successfully updated.",
+
+"update_failure" =>
+"There was an error while trying to update your module to the latest version.",
+
+//----------------------------------------
+//  Morsels
+//----------------------------------------
+
+'morsels' =>
+"Morsels",
+
+'morsels_for_site' =>
+"Morsels for Site:",
+
+'no_morsels' =>
+"No Template Morsels found for this site.",
+
+'add_morsel' =>
+"Add a Morsel",
+
+'edit_morsel' =>
+"Edit a Morsel",
+
+'add_morsel_to_site' =>
+"Add a Morsel to Site: %site_name",
+
+'edit_morsel_for_site' =>
+"Edit a Morsel '%morsel_name' for Site: %site_name",
+
+//----------------------------------------
+//  Morsel delete
+//----------------------------------------
+
+'delete_morsel_confirm' =>
+"Confirm Morsel Deletion",
+
+'morsel_delete_question' =>
+"Are you sure you want to delete %i %count?",
+
+'morsel_deleted' =>
+"1 morsel was successfully deleted.",
+
+'morsels_deleted' =>
+"%i morsels were successfully deleted.",
+
+//----------------------------------------
+//  Morsel edit
+//----------------------------------------
+
+'morsel_name' =>
+"Name",
+
+'morsel_name_subtext' =>
+"The name should only contain alpha-numeric characters, underscores, and dashes.",
+
+'prefix' =>
+"Prefix",
+
+'variable' =>
+"Variable",
+
+'code' =>
+"Code",
+
+'morsel' =>
+"Morsel",
+
+'cached_morsel' =>
+"Cached Morsel",
+
+'click_to_view_cached_morsel' =>
+"Click to View Cached Morsel",
+
+'morsel_refresh' =>
+"Refresh",
+
+'morsel_refresh_explanation' =>
+"Leaving this value at 0 will cause your morsel to only be refreshed by the weblog/channel or template update rules below.",
+
+'morsel_template_refresh' =>
+"Template Refresh",
+
+'morsel_template_refresh_explanation' =>
+"When one of these chosen templates is edited, this morsel will be refreshed.",
+
+'morsel_weblog_refresh' =>
+"Weblog Refresh",
+
+'morsel_channel_refresh' =>
+"Channel Refresh",
+
+'morsel_weblog_refresh_explanation' =>
+"When an entry is published or edited in one of these weblogs, this morsel will be refreshed.",
+
+'morsel_channel_refresh_explanation' =>
+"When an entry is published or edited in one of these channels, this morsel will be refreshed.",
+
+'morsel_category_refresh' =>
+"Category Refresh",
+
+'morsel_category_refresh_explanation' =>
+"When a category is created or edited in one of these category groups, this morsel will be refreshed.",
+
+'rows' =>
+"rows",
+
+'morsel_site' =>
+"Site",
+
+'morsel_refresh_now' =>
+"Refresh Now",
+
+'morsel_cron_refreshing' =>
+"Cron Refreshing",
+
+'morsel_cron_url' =>
+"Cron URL",
+
+'morsel_next_refresh' =>
+"Next Refresh",
+
+'in_minutes' =>
+"(in minutes)",
+
+'morsel_name_required' =>
+"A name is required for all morsels.",
+
+'morsel_name_invalid' =>
+"The name you provided is invalid.",
+
+'morsel_numeric_refresh' =>
+"The morsel refresh interval must be numeric.",
+
+'morsel_name_exists' =>
+"A morsel by the name of '%name%' already exists. Please choose another.",
+
+'morsel_updated' =>
+"Your morsel has been updated and refreshed.",
+
+'morsel_added' =>
+"Your morsel has been added successfully.",
+
+//----------------------------------------
+//  Refresh
+//----------------------------------------
+
+'morsel_cache_id_required' =>
+"No morsel was indicated for refresh. Please go back and try again.",
+
+'morsel_not_found' =>
+"The indicated morsel was not found in the database.",
+
+'morsel_refreshed' =>
+"The morsel '%name' has been refreshed. It will next refresh at %refresh.",
+
+// END
+''=>''
+);
+?>
