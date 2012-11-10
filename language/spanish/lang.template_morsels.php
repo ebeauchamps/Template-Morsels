73ecde2770fd4c481c5b589fdbf2d1373db4fe5f
Michael Van Dorth <pseudoclass@me.com>
2012-11-10 17:47:00 +0100

    Translation to Spanish Traditional (Spain)
    Translation of Template Morsels language file to Spanish. File saved as
    UTF-8

---
+++ language/spanish/lang.template_morsels.php
@@ -0,0 +1,239 @@
+<?php
+
+ /**
+ * Solspace - Template Morsels
+ *
+ * @package		Solspace:Template Morsels
+ * @author		Solspace DevTeam
+ * @copyright	Copyright (c) 2008-2012, Solspace, Inc.
+ * @link		http://solspace.com/docs/addon/c/Template_Morsels/
+ * @version		1.6.0
+ * @filesource 	./system/expressionengine/third_party/template_morsels/language/spanish/
+ */
+
+$L = $lang = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"template_morsels_module_name" =>
+"Template Morsels",
+
+"template_morsels_module_description" =>
+"Bocados de código cacheados exquisitos para sus Plantillas de EE",
+
+'template_morsels_module_version' =>
+'Template Morsels',
+
+//----------------------------------------
+//  Main Menu
+//----------------------------------------
+
+'homepage' =>
+"Página de Inicio",
+
+'manage_morsels' =>
+"Gestionar Bocados",
+
+'online_documentation' =>
+"Documentación en Línea",
+
+//----------------------------------------
+//  Buttons
+//----------------------------------------
+
+'save' =>
+"Guardar",
+
+//----------------------------------------
+//  Errors
+//----------------------------------------
+
+'invalid_request' =>
+"Petición Inválida",
+
+'template_morsels_module_disabled' =>
+"El módulo de Template Morsels está actualmente inhabilitado. Por favor asegure que está instalado y actualizado navegando
+al panel de control del módulo dentro del Panel de Control de ExpressionEngine",
+
+'disable_module_to_disable_extension' =>
+"Para inhabilitar ésta extensión, debe inhabilitar su correspondiente <a href='%url%'>módulo</a>.",
+
+'enable_module_to_enable_extension' =>
+"Para habilitar ésta extensión, debe instalar su correspondiente <a href='%url%'>módulo</a>.",
+
+'cp_jquery_requred' =>
+"La extensión 'jQuery para el Panel de Control' debe estar <a href='%extensions_url%'>habilitado</a> para utilizar éste módulo.",
+
+//----------------------------------------
+//  Update routine
+//----------------------------------------
+
+'update_template_morsels_module' =>
+"Actualizar el módulo Template Morsels",
+
+'template_morsels_update_message' =>
+"Recientemente ha subido una versión nueva de Template Morsels, por favor haga clic aquí para iniciar el script de actualización.",
+
+"update_successful" =>
+"El módulo ha sido actualizado con éxito.",
+
+"update_failure" =>
+"Se ha detectado un error al actualizar el módulo a la última versión.",
+
+//----------------------------------------
+//  Morsels
+//----------------------------------------
+
+'morsels' =>
+"Bocados",
+
+'morsels_for_site' =>
+"Bocados para el Sitio:",
+
+'no_morsels' =>
+"No se ha encontrado Bocados de Platilla para éste sitio.",
+
+'add_morsel' =>
+"Añadir un Bocado",
+
+'edit_morsel' =>
+"Editar un Bocado",
+
+'add_morsel_to_site' =>
+"Añadir un Bocado al Sitio: %site_name",
+
+'edit_morsel_for_site' =>
+"Editar el Bocado '%morsel_name' para el Sitio: %site_name",
+
+//----------------------------------------
+//  Morsel delete
+//----------------------------------------
+
+'delete_morsel_confirm' =>
+"Confirmar Borrado de Bocado",
+
+'morsel_delete_question' =>
+"¿Está seguro que quiere borrar %i %count?",
+
+'morsel_deleted' =>
+"1 bocado ha sido borrado con éxito.",
+
+'morsels_deleted' =>
+"%i bocados han sido borrados con éxito.",
+
+//----------------------------------------
+//  Morsel edit
+//----------------------------------------
+
+'morsel_name' =>
+"Nombre",
+
+'morsel_name_subtext' =>
+"El nombre solo debe contener caracteres alfanuméricas, guiones-bajos, y guiones.",
+
+'prefix' =>
+"Prefijo",
+
+'variable' =>
+"Variable",
+
+'code' =>
+"Código",
+
+'morsel' =>
+"Bocado",
+
+'cached_morsel' =>
+"Bocado Cacheado",
+
+'click_to_view_cached_morsel' =>
+"Haga Clic para Ver Bocados Cacheados",
+
+'morsel_refresh' =>
+"Refrescar",
+
+'morsel_refresh_explanation' =>
+"Dejando éste valor en 0 causará que el bocado solo sea refrescado por el weblog/canal o las reglas de actualización de plantilla abajo.",
+
+'morsel_template_refresh' =>
+"Refrescar por Plantilla",
+
+'morsel_template_refresh_explanation' =>
+"Cuando uno de estas plantillas seleccionadas es editada, éste bocado será refrescado.",
+
+'morsel_weblog_refresh' =>
+"Refrescar por Weblog",
+
+'morsel_channel_refresh' =>
+"Refrescar por Canal",
+
+'morsel_weblog_refresh_explanation' =>
+"Cuando se publica o se edita una entrada en uno de estos weblogs, éste bocado será refrescado.",
+
+'morsel_channel_refresh_explanation' =>
+"Cuando se publica o se edita una entrada en uno de estos canales, éste bocado será refrescado.",
+
+'morsel_category_refresh' =>
+"Refrescar por Categoría",
+
+'morsel_category_refresh_explanation' =>
+"Cuando se crea o se edita una categoría en uno de estos grupos de categorías, éste bocado será refrescado.",
+
+'rows' =>
+"filas",
+
+'morsel_site' =>
+"Sitio",
+
+'morsel_refresh_now' =>
+"Refrescar Ahora",
+
+'morsel_cron_refreshing' =>
+"Refrescar por Cron",
+
+'morsel_cron_url' =>
+"URL de Cron",
+
+'morsel_next_refresh' =>
+"Próxima Refrescar",
+
+'in_minutes' =>
+"(en minutos)",
+
+'morsel_name_required' =>
+"El nombre es obligatorio para todos los bocados.",
+
+'morsel_name_invalid' =>
+"El nombre que ha introducido es inválido.",
+
+'morsel_numeric_refresh' =>
+"El intervalo para refrescar el bocado debe ser numérica.",
+
+'morsel_name_exists' =>
+"Un bocado con el nombre '%name%' ya existe. Por favor elige otra.",
+
+'morsel_updated' =>
+"Su bocado ha sido actualizado y refrescado.",
+
+'morsel_added' =>
+"Su bocado ha sido añadido con éxito.",
+
+//----------------------------------------
+//  Refresh
+//----------------------------------------
+
+'morsel_cache_id_required' =>
+"No se ha indicado ningún bocado para ser refrescado. Por favor vuelve y intente otra vez.",
+
+'morsel_not_found' =>
+"El bocado indicado no fue encontrado en la base de datos.",
+
+'morsel_refreshed' =>
+"El bocado '%name' ha sido refrescado. Será refrescado la próxima vez en %refresh.",
+
+// END
+''=>''
+);
+?>
